7eebe584d27e35b324ff515f34c0b0d6c208fb9b
EC2 Default User <ec2-user@ip-10-0-1-187.us-west-2.compute.internal>
2026-02-02 11:41:39 +0000

    last commit

--- workshop/completed/guardrails/test_basic.py
+++ workshop/completed/guardrails/test_basic.py
@@ -45,7 +45,7 @@ def invoke_nova_lite_with_guardrails(prompt, guardrail_id, guardrail_version, re
 
 prompt = glib.get_prompt_from_command_line()
 # guardrail_id = glib.get_guardrail_id('content_blocking_guardrail_id')
-guardrail_id = '0kfzopdb102o'
+guardrail_id = 'YOUR_GUARDRAIL_ID'
 guardrail_version="DRAFT"
 
 answer = invoke_nova_lite_with_guardrails(prompt, guardrail_id, guardrail_version)
